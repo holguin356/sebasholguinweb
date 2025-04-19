4011e2f4b200a1cfd1308e763b196342e6f7963c
Sebastian Holguin Parra <sebasholguin356@gmail.com>
2025-04-18 21:50:24 -0500

    funcionamiento

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import './style.css'
+import logo from '/src/logo.png'
 
 // Insertamos el contenido HTML para el portafolio en el div con id "app"
 document.querySelector('#app').innerHTML = `
-  <header class="bg-gray-800 text-white py-4">
-    <nav class="max-w-7xl mx-auto flex justify-between items-center px-6">
-      <h1 class="text-2xl font-semibold">Sebastián Holguín</h1>
+  <header class="w-full bg-gray-800 text-white py-4 px-4">
+    <nav class="w-full flex justify-between items-center">
+      <img src="${logo}" alt="logo portafolio" class="logo__nav">
+      <h1 class="text-2xl font-semibold">Sebastián Holguín Dev</h1>
+
       <ul class="flex space-x-6">
         <li><a href="#about" class="hover:text-gray-300">Sobre mí</a></li>
         <li><a href="#projects" class="hover:text-gray-300">Proyectos</a></li>
@@ -15,7 +18,7 @@ document.querySelector('#app').innerHTML = `
 
   <!-- Sección sobre mí -->
   <section id="about" class="bg-gray-100 py-12">
-    <div class="max-w-7xl mx-auto px-6 text-center">
+    <div class="w-full mx-auto px-6 text-center">
       <h2 class="text-3xl font-semibold mb-6">Sobre mí</h2>
       <p class="text-lg text-gray-700">
         Hola, soy Sebastián Holguín, un desarrollador de software con experiencia en HTML, CSS, JavaScript y frameworks modernos. Me apasiona construir soluciones innovadoras y eficientes.
@@ -25,7 +28,7 @@ document.querySelector('#app').innerHTML = `
 
   <!-- Sección de proyectos -->
   <section id="projects" class="py-12">
-    <div class="max-w-7xl mx-auto px-6 text-center">
+    <div class="w-full mx-auto px-6 text-center">
       <h2 class="text-3xl font-semibold mb-6">Mis Proyectos</h2>
       <p class="text-lg text-gray-700">
         Aquí puedes ver algunos de mis proyectos más recientes:
@@ -53,7 +56,7 @@ document.querySelector('#app').innerHTML = `
 
   <!-- Sección de contacto -->
   <section id="contact" class="bg-gray-100 py-12">
-    <div class="max-w-7xl mx-auto px-6 text-center">
+    <div class="w-full mx-auto px-6 text-center">
       <h2 class="text-3xl font-semibold mb-6">Contacto</h2>
       <p class="text-lg text-gray-700">
         Si tienes alguna pregunta o quieres discutir una colaboración, no dudes en ponerte en contacto conmigo.
