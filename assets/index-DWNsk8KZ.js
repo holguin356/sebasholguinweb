57686adea81da32048d4b41505f3bee0fd9f63d3
Sebastian Holguin Parra <sebasholguin356@gmail.com>
2025-04-18 21:47:15 -0500

    Updates

--- assets/index-DWNsk8KZ.js
+++ assets/index-DWNsk8KZ.js
@@ -1,7 +1,9 @@
-(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&r(o)}).observe(document,{childList:!0,subtree:!0});function a(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function r(e){if(e.ep)return;e.ep=!0;const t=a(e);fetch(e.href,t)}})();document.querySelector("#app").innerHTML=`
-  <header class="bg-gray-800 text-white py-4">
-    <nav class="max-w-7xl mx-auto flex justify-between items-center px-6">
-      <h1 class="text-2xl font-semibold">Sebastián Holguín</h1>
+(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&r(o)}).observe(document,{childList:!0,subtree:!0});function a(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function r(e){if(e.ep)return;e.ep=!0;const t=a(e);fetch(e.href,t)}})();const c="/sebasholguinweb/assets/logo-YdmDVDAD.png";document.querySelector("#app").innerHTML=`
+  <header class="w-full bg-gray-800 text-white py-4 px-4">
+    <nav class="w-full flex justify-between items-center">
+      <img src="${c}" alt="logo portafolio" class="logo__nav">
+      <h1 class="text-2xl font-semibold">Sebastián Holguín Dev</h1>
+
       <ul class="flex space-x-6">
         <li><a href="#about" class="hover:text-gray-300">Sobre mí</a></li>
         <li><a href="#projects" class="hover:text-gray-300">Proyectos</a></li>
@@ -12,7 +14,7 @@
 
   <!-- Sección sobre mí -->
   <section id="about" class="bg-gray-100 py-12">
-    <div class="max-w-7xl mx-auto px-6 text-center">
+    <div class="w-full mx-auto px-6 text-center">
       <h2 class="text-3xl font-semibold mb-6">Sobre mí</h2>
       <p class="text-lg text-gray-700">
         Hola, soy Sebastián Holguín, un desarrollador de software con experiencia en HTML, CSS, JavaScript y frameworks modernos. Me apasiona construir soluciones innovadoras y eficientes.
@@ -22,7 +24,7 @@
 
   <!-- Sección de proyectos -->
   <section id="projects" class="py-12">
-    <div class="max-w-7xl mx-auto px-6 text-center">
+    <div class="w-full mx-auto px-6 text-center">
       <h2 class="text-3xl font-semibold mb-6">Mis Proyectos</h2>
       <p class="text-lg text-gray-700">
         Aquí puedes ver algunos de mis proyectos más recientes:
@@ -50,7 +52,7 @@
 
   <!-- Sección de contacto -->
   <section id="contact" class="bg-gray-100 py-12">
-    <div class="max-w-7xl mx-auto px-6 text-center">
+    <div class="w-full mx-auto px-6 text-center">
       <h2 class="text-3xl font-semibold mb-6">Contacto</h2>
       <p class="text-lg text-gray-700">
         Si tienes alguna pregunta o quieres discutir una colaboración, no dudes en ponerte en contacto conmigo.
